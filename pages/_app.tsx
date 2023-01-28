f7f4088d02fbae29cec5925db22a0a782feaa8ff
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2023-01-28 14:27:17 +0200

    Add and configure toast notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-import '@/styles/globals.css';
-import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css";
+import "@/styles/overrides/toast.css";
+import "tailwindcss/tailwind.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        autoClose={1500}
+        closeOnClick={false}
+        hideProgressBar={true}
+        position="top-center"
+      />
+    </>
+  );
 };
 
 export default App;
