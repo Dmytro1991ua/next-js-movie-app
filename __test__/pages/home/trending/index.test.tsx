b90d4a6e15413308a63be05fae089bc9d252be39
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2023-04-14 14:06:29 +0300

    Cover new codebase with tests

---
+++ __test__/pages/home/trending/index.test.tsx
@@ -0,0 +1,81 @@
+import { screen } from "@testing-library/react";
+import { GetServerSidePropsContext } from "next";
+import { QueryObserverSuccessResult } from "react-query";
+import * as hooks from "react-query";
+
+import {
+  mockMovie,
+  mockSessionWithUser,
+  withQueryClientProvider,
+} from "@/mocks/testMocks";
+import { homePageService } from "@/modules/home/home.service";
+import { getServerSideProps } from "@/pages/home/index";
+import TrendingMoviesPage from "@/pages/home/trending";
+import * as utils from "@/utils/utils";
+
+jest.mock("react-query", () => {
+  const originalModule = jest.requireActual("react-query");
+
+  return {
+    ...originalModule,
+    useQuery: jest.fn(),
+  };
+});
+
+jest.mock("uuid", () => {
+  return {
+    v4: jest.fn(() => 1),
+  };
+});
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        user: { name: "John Doe", email: "john@doe.com" },
+        password: "12456",
+      }),
+  })
+);
+
+jest.mock("@/hooks/useGetRandomMovieOrSerial");
+
+describe("TrendingMoviesPage", () => {
+  beforeEach(() => {
+    jest.spyOn(hooks, "useQuery").mockReturnValue({
+      data: {
+        popularMovies: {
+          results: [mockMovie],
+        },
+      },
+    } as unknown as QueryObserverSuccessResult<unknown, unknown>);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should render component without crashing", () => {
+    withQueryClientProvider(<TrendingMoviesPage />);
+
+    expect(screen.getByText(/TrendingMoviesPage/)).toBeInTheDocument();
+  });
+
+  it("Should trigger getServerSideProps and called fetchMoviesForHomePage method within homePageService", async () => {
+    const fetchMoviesForHomePageMock = jest.spyOn(
+      homePageService,
+      "fetchMoviesForHomePage"
+    );
+    const prefetchMovieOrSerialDataMock = jest.spyOn(
+      utils,
+      "prefetchMovieOrSerialData"
+    );
+
+    getServerSideProps({
+      session: mockSessionWithUser,
+    } as unknown as GetServerSidePropsContext);
+
+    expect(prefetchMovieOrSerialDataMock).toHaveBeenCalled();
+    expect(fetchMoviesForHomePageMock).toHaveBeenCalled();
+  });
+});
