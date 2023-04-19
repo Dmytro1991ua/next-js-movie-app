3b6ed3cba79ea0483019a204e49fc1c104c69398
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2023-04-19 19:26:26 +0300

    Adjust env Next auth secret variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -52,7 +52,7 @@ const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
