fb9f18f9a701886e3a1cf80571340abe0400f6a6
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2023-05-07 12:08:30 +0300

    Fix infinite redirect to home page (#80)

--- modules/routes/ProtectedRoutes.tsx
+++ modules/routes/ProtectedRoutes.tsx
@@ -20,12 +20,6 @@ const ProtectedRoutes: FC<ProtectedRoutesProps> = ({
 
   const pathIsProtected = protectedRoutes.indexOf(router.pathname) !== -1;
 
-  useEffect(() => {
-    if (status !== "loading" && !session?.user && pathIsProtected) {
-      router.push(AppRoutes.Home);
-    }
-  }, [pathIsProtected, router, session?.user, status]);
-
   if ((status === "loading" || !session?.user?.id) && pathIsProtected) {
     return <GenerateMainLayoutSkeleton asPath={router.asPath as AppRoutes} />;
   }
